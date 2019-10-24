4c73d405d3b1fd428631d273ee03484befe398a7
Adeniji Adefisayo <adefizzy045@yahoo.com>
2019-10-24 14:17:29 +0100

    removed console.log

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { reducerBucket } from './Components/reducer/index';
 const store = createStore(reducerBucket,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
   );
-store.subscribe(() => console.log(store.getState()));
+
 ReactDOM.render(
   <Provider store = {store}>
     <App />
